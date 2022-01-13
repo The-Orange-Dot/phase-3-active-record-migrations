88fdffd354cd0dce7d19d1589b8dc7a642e5c020
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-14 06:35:51 +0900

    Completed

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Artist.create(name: "BOB", genre: "Yoodling", age: 69, hometown: "Moscow")
+Artist.create(name: "John", genre: "Classical", age: 3, hometown: "Jamaica")
+Artist.create(name: "Fred", genre: "Hiphop", age: 45, hometown: "New York")
